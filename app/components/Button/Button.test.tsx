1603385d5c065eeeb3b147019e4c2b1a68012a01
Sakibul Islam <sakibulislam25800@gmail.com>
2025-02-13 14:30:57 +0000

    chore: prettier fix

--- app/components/Button/Button.test.tsx
+++ app/components/Button/Button.test.tsx
@@ -2,22 +2,22 @@ import { render, screen } from "@testing-library/react";
 import { Button } from "./Button";
 
 it("renders Button with correct text", () => {
-    render(
-        <Button href="#" intent="primary" size="lg">
-            Button
-        </Button>,
-    );
-    expect(screen.getByRole("link")).toHaveTextContent("Button");
+  render(
+    <Button href="#" intent="primary" size="lg">
+      Button
+    </Button>,
+  );
+  expect(screen.getByRole("link")).toHaveTextContent("Button");
 });
 
 it("applies correct classes for primary intent and large size", () => {
-    render(
-        <Button href="#" intent="primary" size="lg">
-            Button
-        </Button>,
-    );
-    const button = screen.getByRole("link");
-    expect(button).toHaveClass(
-        "bg-blue-400 text-white hover:enabled:bg-blue-700 min-w-32 min-h-12 text-lg py-2.5 px-6",
-    );
+  render(
+    <Button href="#" intent="primary" size="lg">
+      Button
+    </Button>,
+  );
+  const button = screen.getByRole("link");
+  expect(button).toHaveClass(
+    "bg-blue-400 text-white hover:enabled:bg-blue-700 min-w-32 min-h-12 text-lg py-2.5 px-6",
+  );
 });
--- jest.config.ts
+++ jest.config.ts
@@ -2,31 +2,31 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-    dir: "./",
+  dir: "./",
 });
 
 const config: Config = {
-    coverageProvider: "v8",
-    testEnvironment: "jsdom",
-    testPathIgnorePatterns: ["<rootDir>/e2e/"],
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageReporters: ["text", "lcov"],
-    reporters: [
-        "default",
-        [
-            "jest-junit",
-            {
-                outputDirectory: ".",
-                outputName: "junit.xml",
-                classNameTemplate: "{classname}",
-                titleTemplate: "{title}",
-                ancestorSeparator: " › ",
-                usePathForSuiteName: true,
-            },
-        ],
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>/e2e/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: ".",
+        outputName: "junit.xml",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+        ancestorSeparator: " › ",
+        usePathForSuiteName: true,
+      },
     ],
+  ],
 };
 
 export default createJestConfig(config);
