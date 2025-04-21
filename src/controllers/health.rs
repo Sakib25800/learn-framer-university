3a37f1dc37e37d3c77a301c2c05c0e6e38fb5356
Sakibul Islam <sakibulislam25800@gmail.com>
2025-04-21 22:13:41 +0100

    WIP: more tests

--- src/controllers/health.rs
+++ src/controllers/health.rs
@@ -28,21 +28,47 @@ mod tests {
     #[sqlx::test]
     async fn health_check(pool: sqlx::PgPool) {
         let (_, anon) = TestApp::init().empty(pool).await;
-
-        let res = anon.get("/").await;
-
-        res.assert_json(&json!({
+        anon.get("/").await.assert_json(&json!({
             "message": "Ok"
         }));
     }
-
-    // #[sqlx::test]
-    // async fn health_check_alt(pool: sqlx::PgPool) {
-    //     run_test(pool, |app, anon| async {
-    //         anon.get("/").await.assert_json(&json!({
-    //             "message": "Ok"
-    //         }));
-    //     })
-    //     .await
-    // }
 }
+
+// async fn signin_should_succeed_with_valid_email() {
+//     let (app, anon) = TestApp::init().empty().await;
+
+//     let sign_in_response = anon
+//         .post("/v1/auth/signin")
+//         .json(&json!({
+//             "email": "new_user@example.com"
+//         }))
+//         .await;
+
+//     sign_in_response.assert_status(StatusCode::OK);
+//     sign_in_response.assert_json(&json!({
+//         "message": "We've sent you an email",
+//     }));
+
+//     let emails = app.emails().await;
+
+//     assert_snapshot!(app.emails_snapshot().await);
+
+//     // Retrieve the continue token
+//     let continue_token = extract_token_from_signin_email(&emails);
+
+//     // Continue with token
+//     let continue_path = format!("/v1/auth/continue/{continue_token}");
+//     let continue_response = anon.get(&continue_path).await;
+
+//     continue_response.assert_status_ok();
+
+//     let json_continue_response = continue_response.json::<serde_json::Value>();
+
+//     match json_continue_response {
+//         serde_json::Value::Object(map) => {
+//             assert!(map.contains_key("access_token"));
+//             assert!(map.contains_key("refresh_token"));
+//         }
+//         _ => panic!("Expected JSON object"),
+//     }
+// }
